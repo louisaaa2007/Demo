69de757e89bac3f765203b3d325dafa00ab60056
louis_lam <louis_lam@WSHK072.talentpro.hk>
2024-04-19 17:21:13 +0800

    19/4 commit

--- demo/src/main/java/repository/UserRepository.java
+++ demo/src/main/java/repository/UserRepository.java
@@ -5,12 +5,15 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import com.demo.user;
+import model.user;
 
 public class UserRepository {
+	
+	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	public Integer add(User user) {
 		KeyHolder keyHolder = new GeneratedKeyHolder();
 		SqlParameterSource sqlParameterSource=new BeanPropertySqlParameterSource(user);
--- demo/src/main/java/service/StockUserService.java
+++ demo/src/main/java/service/StockUserService.java
@@ -1,5 +1,26 @@
 package service;
 
-public class StockUserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import model.User;
+import repository.UserRepository;
+
+public abstract class StockUserService implements UserDetailsService {
+	@Autowired
+	UserRepository userRepository;
+	
+	//載入套件的加密器
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public Integer addUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		Integer user_id = userRepository.add(user);
+		return user_id;
+	}
 }
